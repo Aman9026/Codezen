25cfb30e620617b918e45910cdf4a70d77a0e29a
Aman Gupta <46739111+Aman9026@users.noreply.github.com>
2020-02-23 10:40:06 +0530

    Update PRE4.cpp

--- PRE4.cpp
+++ PRE4.cpp
@@ -22,3 +22,23 @@ Sample Output:
 
 CODE:
 */
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	int div,mod;
+	int ar[n];
+	for(int i=0;i<n;i++)
+	{
+		cin>>ar[i];
+	}
+	int sum1=0,sum2=0;
+	for(int i=0;i<n/2;i++)
+	{
+		int ans1=abs(ar[i]+ar[n-i-1])/10;
+		int ans2=(ar[i]+ar[n-i-1])%10;
+		cout<<ans1<<" "<<ans2<<endl;
+	}
+}
