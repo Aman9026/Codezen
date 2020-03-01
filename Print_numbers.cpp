9aed7bd912eb47b2203c6548110a27164a9bd73c
Aman Gupta <46739111+Aman9026@users.noreply.github.com>
2020-03-01 18:15:02 +0530

    Update Print_numbers.cpp

--- Print_numbers.cpp
+++ Print_numbers.cpp
@@ -1,4 +1,4 @@
-Given the code to print number from 1 to n in increasing order recursively. But it contains few bugs, that you need to rectify such that all the test cases should pass.
+/*Given the code to print number from 1 to n in increasing order recursively. But it contains few bugs, that you need to rectify such that all the test cases should pass.
 Input Format :
 Integer n
 Output Format :
@@ -9,3 +9,16 @@ Sample Input :
  6
 Sample Output :
 1 2 3 4 5 6
+
+ 
+ */
+ 
+ void print(int n){
+    if(n == 1)
+    {
+        cout << n << " ";
+    }
+    else{print(n-1);
+    cout << n << " ";}
+    
+}
