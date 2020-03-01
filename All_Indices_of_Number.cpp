572acf3cf25548d273da04c427f662b7dc5bdabe
Aman Gupta <46739111+Aman9026@users.noreply.github.com>
2020-03-01 19:19:21 +0530

    Update All_Indices_of_Number.cpp

--- All_Indices_of_Number.cpp
+++ All_Indices_of_Number.cpp
@@ -1,3 +1,6 @@
+/*
+Problem:
+
 Given an array of length N and an integer x, you need to find all the indexes where x is present in the input array. Save all the indexes in an array (in increasing order).
 Do this recursively. Indexing in the array starts from 0.
 Input Format :
@@ -14,3 +17,35 @@ Sample Input :
 8
 Sample Output :
 1 3 4
+CODE:
+*/
+
+
+int allIndexes(int input[], int size, int x, int output[]) {
+
+    if (size == 0) { 
+        return 0; 
+    } 
+  
+    // Getting the recursive answer 
+    int smallAns = allIndexes(input + 1, 
+                                    size - 1, x, output); 
+  
+   
+    if (input[0] == x) { 
+        for (int i = smallAns - 1; i >= 0; i--) { 
+            output[i + 1] = output[i] + 1; 
+        } 
+
+        output[0] = 0; 
+        smallAns++; 
+    } 
+    else { 
+
+        for (int i = smallAns - 1; i >= 0; i--) { 
+            output[i] = output[i] + 1; 
+        } 
+    } 
+    return smallAns; 
+
+}
