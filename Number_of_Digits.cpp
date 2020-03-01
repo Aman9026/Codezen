14830125a5f8de2ad3c0b752f3a05b8e38d23829
Aman Gupta <46739111+Aman9026@users.noreply.github.com>
2020-03-01 18:33:52 +0530

    Update Number_of_Digits.cpp

--- Number_of_Digits.cpp
+++ Number_of_Digits.cpp
@@ -1,4 +1,4 @@
-Given the code to find out and return the number of digits present in a number recursively. But it contains few bugs, that you need to rectify such that all the test cases should pass.
+/*Given the code to find out and return the number of digits present in a number recursively. But it contains few bugs, that you need to rectify such that all the test cases should pass.
 Input Format :
 Integer n
 Output Format :
@@ -8,4 +8,21 @@ Constraints :
 Sample Input :
  156
 Sample Output :
-3
+3*/
+
+ int count(int n){
+    static int ct=0;
+     
+    if(n>0)
+    {
+        ct++;
+        count(n/10);
+    }
+    else
+    {
+        return ct;
+    }
+}
+
+
+ 
