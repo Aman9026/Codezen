c71e15c2c2e4112cc04825afb8b682753f834dc6
Aman Gupta <46739111+Aman9026@users.noreply.github.com>
2020-03-08 21:36:36 +0530

    Update Love_for_Characters.cpp

--- Love_for_Characters.cpp
+++ Love_for_Characters.cpp
@@ -19,3 +19,20 @@ Sample output:
 
 CODE:
 */
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a=0,s=0,p=0;
+    int n=0;
+    cin>>n;
+    char * str = new char [n];
+    cin>>str;
+    for(int i=0;i<n;i++){
+        if(str[i] =='a'){a++;}
+        else if(str[i]=='s'){s++;}
+        else if(str[i]=='p'){p++;}   
+    }
+    cout<<a<<" "<<s<<" "<<p;
+	return 0;
+}
