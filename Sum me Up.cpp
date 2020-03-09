d7162345748cc2261961ccc6d7130be4e82ae1c2
Aman Gupta <46739111+Aman9026@users.noreply.github.com>
2020-03-09 11:18:36 +0530

    Update and rename test/Sum me Up to Sum me Up.cpp

--- Sum me Up.cpp
+++ Sum me Up.cpp
@@ -1,3 +1,6 @@
+/*
+PROBLEM:
+
 There will be ‘t’ test cases having an integer. You have to sum up all the digits of this integer. For e.g. For 6754, the answer will be 6 + 7 + 5 + 4 = 22.
 Input Format:
 First line will have an integer ‘t’ denoting the number of test cases.
@@ -17,3 +20,29 @@ Sample Output:
 Explanation:
 1 + 5 + 4 + 7 = 17
 4 + 5 + 8 + 7 + 6 = 30
+
+
+CODE:
+
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef unsigned long long ull;
+int main() {
+
+  ull t;
+  cin>>t;
+  while(t--){
+    ull val;
+    cin>>val;
+    ull s = 0;
+    while(val!=0){
+      s+=val%10;
+      val/=10;
+    }
+    cout<<s<<endl;
+  }
+  return 0;
+}
