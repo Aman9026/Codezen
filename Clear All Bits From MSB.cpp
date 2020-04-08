fbb5620186ab1930774653d651b973538890bc87
Aman Gupta <46739111+Aman9026@users.noreply.github.com>
2020-04-08 09:49:57 +0530

    Update Clear All Bits From MSB.cpp

--- Clear All Bits From MSB.cpp
+++ Clear All Bits From MSB.cpp
@@ -20,3 +20,14 @@ Sample Output 2 :
 
 CODE:
 */
+
+
+int clearAllBits(int n, int i){
+    /* Don't write main().
+     * Don't read input, it is passed as function argument.
+     * Return output and don't print it.
+     * Taking input and printing output is handled automatically.
+     */
+  int m = (1<<i)-1;
+  return n&m;   
+}
