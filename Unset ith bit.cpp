297d301ce841af0fc09c702ee2424d8f21ee5b07
Aman Gupta <46739111+Aman9026@users.noreply.github.com>
2020-04-08 09:43:57 +0530

    Update Unset ith bit.cpp

--- Unset ith bit.cpp
+++ Unset ith bit.cpp
@@ -1,6 +1,19 @@
 /*
 PROBLEM:
-
+You are given two integers N and i. You need to make ith bit of binary representation of N to 0 and return the updated N.
+Counting of bits start from 0 from right to left.
+Input Format :
+Two integers N and i (separated by space)
+Output Format :
+Updated N
+Sample Input 1 :
+7 2
+Sample Output 1 :
+3
+Sample Input 2 :
+12 1
+Sample Output 2 :
+12
 
 
 CODE:
